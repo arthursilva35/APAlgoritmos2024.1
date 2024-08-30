3815ae4567ab9ffac79d4c115d32a02aa9837b9c
Inovar para Pessoas Negras <silvaarthur@ufba.br>
2024-08-30 12:14:32 -0300

    comecando contests in sequence

---
+++ contests_in_sequence.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> v[n];
+
+    for(int i = 0; i < n; i++){
+        int cur;
+        cin >> cur;
+        v.push_back(cur);
+    }
+
+    for(int i = 0; i < m; i++){
+        
+    }
+}
