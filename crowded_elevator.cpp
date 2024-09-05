81096b71b2e59dafb27cfce0ac564b448a6d5484
Inovar para Pessoas Negras <silvaarthur@ufba.br>
2024-09-05 14:59:47 -0300

    comecando elevator

---
+++ crowded_elevator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void imprimeVetor(vector<string> &vetor){
+    for(int i = 0; i < vetor.size(); i++){
+        cout << vetor[i] << " ";
+    }
+}
+
+
+
+int main(){
+    int t;
+
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        int n, c, m;
+
+        vector<int> andaresParaVisitar;
+        
+        cin >> n >> c >> m;
+
+        for(int i = 0; i < m; i++){
+
+            
+        }
+    }
+    return 0; 
+}
