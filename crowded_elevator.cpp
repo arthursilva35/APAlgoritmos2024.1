29064703c49cb8462e376c7e737b2632b06dfffa
Inovar para Pessoas Negras <silvaarthur@ufba.br>
2024-09-05 17:10:13 -0300

    solução crowded elevator

--- crowded_elevator.cpp
+++ crowded_elevator.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-void imprimeVetor(vector<string> &vetor){
+void imprimeVetor(vector<int> &vetor){
     for(int i = 0; i < vetor.size(); i++){
         cout << vetor[i] << " ";
     }
@@ -15,7 +15,10 @@ int main(){
     int t;
 
     cin >> t;
+    
     for(int i = 0; i < t; i++){
+        int energia = 0;
+        
         int n, c, m;
 
         vector<int> andaresParaVisitar;
@@ -24,8 +27,30 @@ int main(){
 
         for(int i = 0; i < m; i++){
 
-            
+            int andar;
+
+            cin >> andar;
+
+            andaresParaVisitar.push_back(andar);            
         }
+
+        sort(andaresParaVisitar.begin(), andaresParaVisitar.end());
+
+        while(!andaresParaVisitar.empty()){
+            int tam = andaresParaVisitar.size();
+            
+            energia += andaresParaVisitar[tam - 1]*2;
+
+            for(int j = 0; j < c; j++){
+                if(!andaresParaVisitar.empty()){
+                    
+                    andaresParaVisitar.pop_back();
+                }
+                
+            }
+        }
+
+        cout << energia << endl;
     }
     return 0; 
 }
